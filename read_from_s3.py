ee5558f2c300787d0eba9a29b1161f464d30a8b5
akshat <lit2021033@iiitl.ac.in>
2024-04-09 01:36:27 +0530

    changes in s3 file

--- read_from_s3.py
+++ read_from_s3.py
@@ -1,26 +1,41 @@
 import boto3
+from PyPDF2 import PdfReader
+from io import BytesIO
 import textract
 
 class S3PDFExtractor:
     def __init__(self, bucket_name, folder_prefix=''):
         self.bucket_name = bucket_name
         self.folder_prefix = folder_prefix
+        client = boto3.client('s3')
+
+    def extract_text_from_pdf(self, pdf_file):
+        s3_client = boto3.client('s3')
+        response = s3_client.get_object(Bucket=self.bucket_name, Key=pdf_file)
+
+        pdf_data = response['Body'].read()
+
+        pdf_reader = PdfReader(BytesIO(pdf_data))
+
+        text = ''
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+        return text
+
 
     def list_pdf_files(self):
         s3_client = boto3.client('s3')
-        paginator = s3_client.get_paginator('list_objects_v2')
-        pages = paginator.paginate(Bucket=self.bucket_name, Prefix=self.folder_prefix)
-        pdf_files = []
-        for page in pages:
-            for obj in page['Contents']:
+        try:
+            response = s3_client.list_objects_v2(Bucket=self.bucket_name)
+
+            pdf_files = []
+            for obj in response['Contents']:
                 key = obj['Key']
                 if key.endswith('.pdf'):
                     pdf_files.append(key)
-        return pdf_files
 
-    def extract_text_from_pdf(self, pdf_filename):
-        s3_client = boto3.client('s3')
-        response = s3_client.get_object(Bucket=self.bucket_name, Key=pdf_filename)
-        text = textract.process(response['Body'], method='pdftotext')
-        return text.decode('utf-8')
+            return pdf_files
 
+        except Exception as e:
+            # print("Error listing PDF files in S3 bucket:", str(e))
+            return None
